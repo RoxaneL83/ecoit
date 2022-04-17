d330593f99c3ab09970634055e0036b18eb73bc8
RoxaneL83 <lambert.roxane@hotmail.fr>
2022-04-17 13:04:56 +0200

    Add file header.php and integration in file index.php

---
+++ header.php
@@ -0,0 +1,50 @@
+<!-- Barre de navigation -->
+      <nav class="navbar navbar-expand-md">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img
+              src="img/logo_ecoit.png"
+              alt="Logo Ecoit"
+              width="30"
+              height="24"
+              class="d-inline-block align-text-top"
+            />
+            <img
+              id="ecoit"
+              src="img/name-ecoit.png"
+              alt="Ecoit"
+              width="60"
+              height="48"
+            />
+          </a>
+          <button
+            class="navbar-toggler bg-light"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Formations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Devenir formateur(trice)</a>
+              </li>
+            </ul>
+            <a href="connexion.php" class="btn btn-outline-light me-2">Se connecter</a>
+            <a href="/inscription" class="btn btn-light">S'inscrire</a>
+          </div>
+        </div>
+      </nav>
